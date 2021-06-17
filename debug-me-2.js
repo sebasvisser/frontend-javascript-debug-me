3ea7b1119f570516c92d10beb46b8737b5c3cb37
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-06-17 16:07:25 +0200

    debug2 fixed

--- debug-me-2.js
+++ debug-me-2.js
@@ -1,13 +1,13 @@
 const height = 1.79;
 const weight = 56;
 
-const BMI = weight / (heigth * heigth);
+const BMI = weight / (height * height); //typo in variabelenaam
 
 console.log("Your BMI is: " + BMI.toFixed());
 
 if (BMI > 25) {
-  console.log('Depending on your build, you might be overweight");
-} else (BMI < 18) {
+  console.log("Depending on your build, you might be overweight");// wisselende " en ' gebruikt
+} else if (BMI < 18) { //if vergeten
   console.log("Depending on your build, you might be underweight");
 } else {
   console.log("Looks like you're height / weight ratio is pretty healthy");
