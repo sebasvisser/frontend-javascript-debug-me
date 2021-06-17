211fd0b9227a6b18f263230de3286fee35585c1e
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-06-17 16:08:55 +0200

    fix debug3

--- debug-me-3.js
+++ debug-me-3.js
@@ -1,6 +1,6 @@
 const weight = 110;
-const BMI = weight \ (height * height);
-const height = 1.79;
+const height = 1.79; // deze moet voor BMI variable gedeclareerd zijn
+const BMI = weight / (height * height); //operator verkeerd om \ ipv /
 
 console.log("Your BMI is: " + BMI.toFixed());
 
