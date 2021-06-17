b19d217e6b761c39144ab689b93f1f8a0ceff7b5
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-06-17 16:12:23 +0200

    debug4 fixed

--- debug-me-4.js
+++ debug-me-4.js
@@ -1,17 +1,18 @@
 const weight = 110;
-cost height = 1.79;
+const height = 1.79; //typo in variabele type
 const BMI = weight / (height * height);
 
-const outcome;
+let outcome = " "; //vergeten te initialiseren
 
 console.log("Your BMI is: " + BMI.toFixed());
 
 if (BMI < 25) {
-  outcome = "Depending on your build, you might be overweight";
+  outcome = "Depending on your build, you might be overweight"; // outcome
+  // moet een let zijn om te kunnen wijzigen
 } else if (BMI < 18) {
   outcome = "Depending on your build, you might be underweight";
 } else {
-  outcome = ("Looks like you're height / weight ratio is pretty healthy";
+  outcome = "Looks like you're height / weight ratio is pretty healthy"; // haakje teveel in begin zin
 }
 
 console.log(outcome);
