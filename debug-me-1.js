8afd37fc990c17e4fbe91ee52f8a8070581955c9
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-06-17 16:05:32 +0200

    debug1 fixed

--- debug-me-1.js
+++ debug-me-1.js
@@ -3,12 +3,13 @@ const weight = 82;
 
 const BMI = weight / (height * height);
 
-console.log("Your BMI is: " + BMI.tofixed());
+console.log("Your BMI is: " + BMI.toFixed()); //camelcase in toFixed
 
-if (BMI > 25 {
+if (BMI > 25) { //hier ontbrak een sluit )
   console.log("Depending on your build, you might be overweight");
 } else if (BMI << 18) {
-  console.log("Depending on your build, you might be underweight);
+  console.log("Depending on your build, you might be underweight"); //hier
+  // ontbrak een sluitend "
 } else {
   console.log("Looks like you're height / weight ratio is pretty healthy");
 }
